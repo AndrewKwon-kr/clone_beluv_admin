e6639518ff3f0182c871407f7f93e0dfa909663f
AndrewKwon <hj.kwon@mayoube.co.kr>
2022-09-07 16:05:22 +0900

    들여쓰기 맞춤

--- src/views/BeluvTvPage.vue
+++ src/views/BeluvTvPage.vue
@@ -1,21 +1,13 @@
 <template>
-  <v-container
-    id="user-profile-view"
-    fluid
-    tag="section"
-  >
+  <v-container id="user-profile-view" fluid tag="section">
     <h1>총 {{data.length}} 건</h1>
-    <a-table 
-    :columns="columns" 
-    :data-source="data" 
-    :rowKey="(record, index) => { return index }"
-    >
+    <a-table :columns="columns" :data-source="data" :rowKey="(record, index) => { return index }">
       <div slot="thumbnails" slot-scope="thumbnails" class="thumbnail">
         <a-popover trigger="click">
           <template slot="content">
-            <img :src=thumbnails v-bind:style="{width: '300px', height: '200px'}"/>
+            <img :src=thumbnails v-bind:style="{width: '300px', height: '200px'}" />
           </template>
-            <img :src=thumbnails v-bind:style="{width: '30px', height: '20px', cursor: 'pointer'}"/>
+          <img :src=thumbnails v-bind:style="{width: '30px', height: '20px', cursor: 'pointer'}" />
         </a-popover>
       </div>
       <div slot="summary" slot-scope="summary">
@@ -31,7 +23,7 @@
       </div>
       <div slot="status" slot-scope="status">
         <v-icon v-if="status === 'ACTIVATE'" color="green darken-2">mdi-eye</v-icon>
-        <v-icon v-else >mdi-eye-off</v-icon>
+        <v-icon v-else>mdi-eye-off</v-icon>
       </div>
       <span slot="createdAt" slot-scope="createdAt">{{createdAt.substr(0,10)}}</span>
     </a-table>
@@ -39,139 +31,141 @@
 </template>
 
 <script>
-  import { dummys } from '../dummys/dummys.js'
+import { dummys } from '../dummys/dummys.js'
 
-  export default { 
-    name: 'ProductListView',
-    data() {
-      
-        const columns = [
-  {
-    title: "No",
-    dataIndex: 'id',
-    key: 'id',
-  },
-  {
-    title: '대카테고리',
-    dataIndex: 'categoryName',
-    key: 'categoryName',
-    width: '10%',
-    filters: [
-      { text: '육아꿀팁', value: '육아꿀팁' },
-      { text: '육아정보', value: '육아정보' },
-      { text: '브이로그/일상', value: '브이로그/일상' },
-    ],
-    onFilter: (value, record) => record.categoryName.indexOf(value) === 0,
-  },
-  {
-    title: '소카테고리',
-    dataIndex: 'subCategoryName',
-    key: 'subCategoryName',
-    width: '10%',
-    filters: [
-      { text: '건강', value: '건강' },
-      { text: '육아정보', value: '육아정보' },
-      { text: '놀이', value: '놀이' },
-      { text: '간식/식품', value: '간식/식품' },
-      { text: '교육', value: '교육' },
-    ],
-    onFilter: (value, record) => record.subCategoryName.indexOf(value) === 0,
-  },
-  {
-    title: '썸네일',
-    key: 'thumbnails',
-    width: '10%',
-    dataIndex: 'thumbnails',
-    scopedSlots: {customRender: 'thumbnails'}
-  },
-  {
-    title: '채널명',
-    dataIndex: 'channel_title',
-    key: 'channel_title',
-  },
-  {
-    title: '영상제목',
-    dataIndex: 'title',
-    key: 'title',
-    customRender: (text, record) => {
-      return (
-        <div slot="title" slot-scope="title">
-          <span>{{ text }}</span>
-          <button
-            icon
-            x-small
-            target="_blank"
-            onClick={() => window.open(record.videoLink)}
-          >
-          <img src={require('@/assets/open-in-new.png')} style={{width: '16px'}}/>
-          </button>
-      </div>
-      )
-    } 
-    // scopedSlots: {customRender: 'title'},
-  },
-  {
-    title: '요약글',
-    dataIndex: 'summary',
-    key: 'summary',
-    width: '8%',
-    scopedSlots: {customRender: 'summary'}
-  },
-  {
-    title: '조회수',
-    dataIndex: 'playCount',
-    key: 'playCount',
-    width: '8%',
-    scopedSlots: {customRender: 'playCount'},
-    sorter: (a, b) => a.playCount - b.playCount,
-    sortDirections: ['descend', 'ascend'],
-  },
-  {
-    title: '활성',
-    dataIndex: 'status',
-    key: 'status',
-    width: '7%',
-    scopedSlots: {customRender: 'status'},
-    filters: [
-      { text: '활성', value: 'ACTIVATE' },
-      { text: '비활성', value: 'INACTIVATE' },
-    ],
-    filterMultiple: false,
-    onFilter: (value, record) => record.status.indexOf(value) === 0,
-  },
-  {
-    title: '재생시간',
-    dataIndex: 'play_time',
-    key: 'play_time',
-    width: '10%',
-  },
-  {
-    title: '등록날짜',
-    dataIndex: 'createdAt',
-    key: 'createdAt',
-    width: '7%',
-    scopedSlots: {customRender: 'createdAt'}
+export default {
+  name: 'ProductListView',
+  data() {
 
-  },
-]
-        return {
-
-          columns: columns,
-          data: dummys
+    const columns = [
+      {
+        title: "No",
+        dataIndex: 'id',
+        key: 'id',
+      },
+      {
+        title: '대카테고리',
+        dataIndex: 'categoryName',
+        key: 'categoryName',
+        width: '10%',
+        filters: [
+          { text: '육아꿀팁', value: '육아꿀팁' },
+          { text: '육아정보', value: '육아정보' },
+          { text: '브이로그/일상', value: '브이로그/일상' },
+        ],
+        onFilter: (value, record) => record.categoryName.indexOf(value) === 0,
+      },
+      {
+        title: '소카테고리',
+        dataIndex: 'subCategoryName',
+        key: 'subCategoryName',
+        width: '10%',
+        filters: [
+          { text: '건강', value: '건강' },
+          { text: '육아정보', value: '육아정보' },
+          { text: '놀이', value: '놀이' },
+          { text: '간식/식품', value: '간식/식품' },
+          { text: '교육', value: '교육' },
+        ],
+        onFilter: (value, record) => record.subCategoryName.indexOf(value) === 0,
+      },
+      {
+        title: '썸네일',
+        key: 'thumbnails',
+        width: '10%',
+        dataIndex: 'thumbnails',
+        scopedSlots: { customRender: 'thumbnails' }
+      },
+      {
+        title: '채널명',
+        dataIndex: 'channel_title',
+        key: 'channel_title',
+      },
+      {
+        title: '영상제목',
+        dataIndex: 'title',
+        key: 'title',
+        customRender: (text, record) => {
+          return (
+            <div slot="title" slot-scope="title">
+              <span>{{ text }}</span>
+              <button
+                icon
+                x-small
+                target="_blank"
+                onClick={() => window.open(record.videoLink)}
+              >
+                <img src={require('@/assets/open-in-new.png')} style={{ width: '16px' }} />
+              </button>
+            </div>
+          )
         }
-      }
-    
+        // scopedSlots: {customRender: 'title'},
+      },
+      {
+        title: '요약글',
+        dataIndex: 'summary',
+        key: 'summary',
+        width: '8%',
+        scopedSlots: { customRender: 'summary' }
+      },
+      {
+        title: '조회수',
+        dataIndex: 'playCount',
+        key: 'playCount',
+        width: '8%',
+        scopedSlots: { customRender: 'playCount' },
+        sorter: (a, b) => a.playCount - b.playCount,
+        sortDirections: ['descend', 'ascend'],
+      },
+      {
+        title: '활성',
+        dataIndex: 'status',
+        key: 'status',
+        width: '7%',
+        scopedSlots: { customRender: 'status' },
+        filters: [
+          { text: '활성', value: 'ACTIVATE' },
+          { text: '비활성', value: 'INACTIVATE' },
+        ],
+        filterMultiple: false,
+        onFilter: (value, record) => record.status.indexOf(value) === 0,
+      },
+      {
+        title: '재생시간',
+        dataIndex: 'play_time',
+        key: 'play_time',
+        width: '10%',
+      },
+      {
+        title: '등록날짜',
+        dataIndex: 'createdAt',
+        key: 'createdAt',
+        width: '7%',
+        scopedSlots: { customRender: 'createdAt' }
+
+      },
+    ]
+    return {
+
+      columns: columns,
+      data: dummys
+    }
   }
+
+}
 </script>
 <style>
-  .ant-table-title {
-    display: none;
-  }
-  .ant-table-thead > tr > th {
-    text-align: center;
-  }
-  .ant-table-tbody {
-    background-color: white;
-    text-align: center;
-  }
+.ant-table-title {
+  display: none;
+}
+
+.ant-table-thead>tr>th {
+  text-align: center;
+}
+
+.ant-table-tbody {
+  background-color: white;
+  text-align: center;
+}
 </style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,64 +1,42 @@
 <template>
-  <v-container
-    id="yarn-view"
-    fluid
-    tag="section"
-  >
+  <v-container id="yarn-view" fluid tag="section">
     <v-row>
-      <v-col
-        v-for="({ actionIcon, actionText, ...attrs }, i) in stats"
-        :key="i"
-        cols="12"
-        md="6"
-        lg="3"
-      >
+      <v-col v-for="({ actionIcon, actionText, ...attrs }, i) in stats" :key="i" cols="12" md="6" lg="3">
         <material-stat-card v-bind="attrs">
           <template #actions>
-            <v-icon
-              class="mr-2"
-              small
-              v-text="actionIcon"
-            />
+            <v-icon class="mr-2" small v-text="actionIcon" />
             <div class="text-truncate">
               {{ actionText }}
             </div>
           </template>
         </material-stat-card>
       </v-col>
-      
+
     </v-row>
-    <LineChartGenerator
-        :chart-options="chartOptions"
-        :chart-data="chartData"
-        :chart-id="chartId"
-        :dataset-id-key="datasetIdKey"
-        :plugins="plugins"
-        :css-classes="cssClasses"
-        :styles="styles"
-        :width="width"
-        :height="height"
-      />
+    <LineChartGenerator :chart-options="chartOptions" :chart-data="chartData" :chart-id="chartId"
+      :dataset-id-key="datasetIdKey" :plugins="plugins" :css-classes="cssClasses" :styles="styles" :width="width"
+      :height="height" />
   </v-container>
 </template>
 
 <script>
-  // Utilities
-  import { get } from 'vuex-pathify'
-  import { HOST, headers } from '../http-api/index'
-  import { Line as LineChartGenerator } from 'vue-chartjs/legacy'
+// Utilities
+import { get } from 'vuex-pathify'
+import { HOST, headers } from '../http-api/index'
+import { Line as LineChartGenerator } from 'vue-chartjs/legacy'
 
-  import {
-    Chart as ChartJS,
-    Title,
-    Tooltip,
-    Legend,
-    LineElement,
-    LinearScale,
-    CategoryScale,
-    PointElement
-  } from 'chart.js'
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  LineElement,
+  LinearScale,
+  CategoryScale,
+  PointElement
+} from 'chart.js'
 
-  ChartJS.register(
+ChartJS.register(
   Title,
   Tooltip,
   Legend,
@@ -67,30 +45,30 @@
   CategoryScale,
   PointElement
 )
-  
-  export default {
-    name: 'DashboardView',
-    components: {
-      LineChartGenerator
-    },
-    methods: {
-      async getData() {
-        const res = await this.$axios.get(`${HOST}/user/count`, {
-          headers: headers()
-        });
 
-        this.items = res.data.result;
-        this.stats = this.stats.map((stat) => {
-          return { ...stat, value: Intl.NumberFormat().format(this.items[stat.value]) }
-        })
-      },
+export default {
+  name: 'DashboardView',
+  components: {
+    LineChartGenerator
+  },
+  methods: {
+    async getData() {
+      const res = await this.$axios.get(`${HOST}/user/count`, {
+        headers: headers()
+      });
 
-    },
-    async mounted() {
-      await this.getData();
+      this.items = res.data.result;
+      this.stats = this.stats.map((stat) => {
+        return { ...stat, value: Intl.NumberFormat().format(this.items[stat.value]) }
+      })
     },
 
-    props: {
+  },
+  async mounted() {
+    await this.getData();
+  },
+
+  props: {
     chartId: {
       type: String,
       default: 'line-chart'
@@ -113,96 +91,96 @@
     },
     styles: {
       type: Object,
-      default: () => {}
+      default: () => { }
     },
     plugins: {
       type: Array,
       default: () => []
     }
   },
-    
-    
-    data: () => ({
-      items: {},
-      stats: [
-        {
-          actionIcon: 'mdi-alpha-a-box-outline',
-          actionText: '10만 가즈아...!',
-          color: '#FD9A13',
-          icon: 'mdi-account-multiple',
-          title: '총 회원수',
-          value: 'all'
-        },
-        {
-          actionIcon: 'mdi-alpha-t-box-outline',
-          actionText: '오늘 가입해주신 분들',
-          color: 'primary',
-          icon: 'mdi-account-arrow-up',
-          title: '오늘 가입회원',
-          value: 'today'
-        },
-        {
-          actionIcon: 'mdi-alpha-w-box-outline',
-          actionText: '이번주 가입자분들',
-          color: 'success',
-          icon: 'mdi-account-multiple-plus',
-          title: '이번주 가입회원',
-          value: 'week'
-        },
-        {
-          actionIcon: 'mdi-alpha-m-box-outline',
-          actionText: '이번달 가입자분들',
-          color: 'info',
-          icon: 'mdi-account-group',
-          title: '이번달 가입회원',
-          value: 'month'
-        },
-        {
-          actionIcon: 'mdi-history',
-          actionText: '돌아와요',
-          color: 'info',
-          icon: 'mdi-account-multiple-minus',
-          title: '탈퇴자',
-          value: 'inActivatedUser'
-        },
+
+
+  data: () => ({
+    items: {},
+    stats: [
+      {
+        actionIcon: 'mdi-alpha-a-box-outline',
+        actionText: '10만 가즈아...!',
+        color: '#FD9A13',
+        icon: 'mdi-account-multiple',
+        title: '총 회원수',
+        value: 'all'
+      },
+      {
+        actionIcon: 'mdi-alpha-t-box-outline',
+        actionText: '오늘 가입해주신 분들',
+        color: 'primary',
+        icon: 'mdi-account-arrow-up',
+        title: '오늘 가입회원',
+        value: 'today'
+      },
+      {
+        actionIcon: 'mdi-alpha-w-box-outline',
+        actionText: '이번주 가입자분들',
+        color: 'success',
+        icon: 'mdi-account-multiple-plus',
+        title: '이번주 가입회원',
+        value: 'week'
+      },
+      {
+        actionIcon: 'mdi-alpha-m-box-outline',
+        actionText: '이번달 가입자분들',
+        color: 'info',
+        icon: 'mdi-account-group',
+        title: '이번달 가입회원',
+        value: 'month'
+      },
+      {
+        actionIcon: 'mdi-history',
+        actionText: '돌아와요',
+        color: 'info',
+        icon: 'mdi-account-multiple-minus',
+        title: '탈퇴자',
+        value: 'inActivatedUser'
+      },
+    ],
+    chartData: {
+      labels: [
+        '1월',
+        '2월',
+        '3월',
+        '4월',
+        '5월',
+        '6월',
+        '7월',
+        '8월',
       ],
-      chartData: {
-        labels: [
-          '1월',
-          '2월',
-          '3월',
-          '4월',
-          '5월',
-          '6월',
-          '7월',
-          '8월',
-        ],
-        datasets: [
-          {
-            label: '가입(샘플)',
-            backgroundColor: '#f87979',
-            borderColor: '#f87979',
-            data: [40, 39, 10, 40, 39, 80, 40, 90]
-          },
-          {
-            label: '탈퇴(샘플)',
-            backgroundColor: '#5B82E3',
-            borderColor: '#5B82E3',
-            data: [20, 9, 10, 14, 6, 16, 30, 8]
-          }
-        ]
-      },
-      chartOptions: {
-        responsive: true,
-        maintainAspectRatio: false
-      }
-    
-    }),
-    computed: {
-      sales: get('sales/sales'),
-      totalSales () {
-        return this.sales.reduce((acc, val) => acc + val.salesInM, 0)
-      },
+      datasets: [
+        {
+          label: '가입(샘플)',
+          backgroundColor: '#f87979',
+          borderColor: '#f87979',
+          data: [40, 39, 10, 40, 39, 80, 40, 90]
+        },
+        {
+          label: '탈퇴(샘플)',
+          backgroundColor: '#5B82E3',
+          borderColor: '#5B82E3',
+          data: [20, 9, 10, 14, 6, 16, 30, 8]
+        }
+      ]
     },
-  }
+    chartOptions: {
+      responsive: true,
+      maintainAspectRatio: false
+    }
+
+  }),
+  computed: {
+    sales: get('sales/sales'),
+    totalSales() {
+      return this.sales.reduce((acc, val) => acc + val.salesInM, 0)
+    },
+  },
+}
 </script>
