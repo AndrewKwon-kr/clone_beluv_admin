a1088a8ef7b4b2de7eb4bc6bcf51b88da7991d05
AndrewKwon <hj.kwon@mayoube.co.kr>
2022-09-08 15:38:28 +0900

    datePicker 추가

--- src/views/ReviewPage.vue
+++ src/views/ReviewPage.vue
@@ -1,8 +1,14 @@
 <template>
   <v-container id="user-profile-view" fluid tag="section">
-    <h1>총 {{data.length}} 건</h1>
-    <a-table :columns="columns" :data-source="data" :rowKey="(record, index) => { return index }">
-      <div slot="ProductReview_Url" slot-scope="ProductReview_Url" >
+    <h1>총 {{filteredData.length}} 건</h1>
+    <div class="date-picker">
+      <a-date-picker v-model="startValue" :disabled-date="disabledStartDate" format="YYYY-MM-DD"
+        placeholder="시작일" @openChange="handleStartOpenChange" />
+      <a-date-picker v-model="endValue" :disabled-date="disabledEndDate" format="YYYY-MM-DD"
+        placeholder="종료일" :open="endOpen" @openChange="handleEndOpenChange" />
+    </div>
+    <a-table :columns="columns" :data-source="filteredData" :rowKey="(record, index) => { return index }">
+      <div slot="ProductReview_Url" slot-scope="ProductReview_Url">
         <a-popover trigger="click">
           <template slot="content">
             <img :src=ProductReview_Url v-bind:style="{width: '600px', height: '400px'}" />
@@ -13,7 +19,27 @@
       <div slot="rating" slot-scope="rating">
         <a-rate :value=rating disabled style="fontSize: 10px" />
       </div>
-      <span slot="createdAt" slot-scope="createdAt">{{createdAt.substr(0,10)}}</span>
+      <div slot="ProductReview_content" slot-scope="ProductReview_content">
+        <a-tooltip placement="bottom">
+          <template slot="title">
+            <span>{{ ProductReview_content }}</span>
+          </template>
+          <span>{{ ProductReview_content.substr(0, 30) }}...</span>
+          <span class="text-bold">({{ ProductReview_content.length }})</span>
+          <v-icon color="green darken-2">mdi-card-text</v-icon>
+        </a-tooltip>
+      </div>
+      <div slot="ProductReview_Content_bad" slot-scope="ProductReview_Content_bad">
+        <a-tooltip placement="bottom">
+          <template slot="title">
+            <span>{{ ProductReview_Content_bad }}</span>
+          </template>
+          <span>{{ ProductReview_Content_bad.substr(0, 30) }}...</span>
+          <span class="text-bold" v-if="ProductReview_Content_bad">({{ ProductReview_Content_bad.length }})</span>
+          <v-icon color="green darken-2">mdi-card-text</v-icon>
+        </a-tooltip>
+      </div>
+      <span slot="createdAt" slot-scope="createdAt">{{ createdAt.substr(0,10) }}</span>
       <div slot="status" slot-scope="status">
         <a-switch v-if="status==='ACTIVATE'" default-checked />
         <a-switch v-else />
@@ -23,10 +49,49 @@
 </template>
 
 <script>
-import { dummys } from '../dummys/ReviewList'
+import { dummys } from '../dummys/ReviewList';
+import moment from 'moment';
 
 export default {
   name: 'ReviewView',
+  methods: {
+    moment,
+    disabledStartDate(startValue) {
+      const endValue = this.endValue;
+      if (!startValue || !endValue) {
+        return false;
+      }
+      return startValue.valueOf() > endValue.valueOf();
+    },
+    disabledEndDate(endValue) {
+      const startValue = this.startValue;
+      if (!endValue || !startValue) {
+        return false;
+      }
+      return startValue.valueOf() >= endValue.valueOf();
+    },
+    handleStartOpenChange(open) {
+      if (!open) {
+        this.endOpen = true;
+      }
+    },
+    handleEndOpenChange(open) {
+      this.endOpen = open;      
+    },
+  },
+  watch: {
+    endOpen: function() {
+      if (!this.endOpen) {
+        this.filteredData = this.data.filter((review) => {
+          let createDate = moment(review.createdAt);
+          let startDate = moment(this.startValue);
+          let endDate = moment(this.endValue);
+
+          return moment(createDate).isBetween(startDate, endDate);
+        })
+      }
+    }
+  },
   data() {
 
     const columns = [
@@ -41,6 +106,17 @@ export default {
         dataIndex: 'category_Name',
         key: 'category_Name',
         width: '8%',
+        filters: [
+          { text: '간식', value: '간식' },
+          { text: '기저귀', value: '기저귀' },
+          { text: '물티슈', value: '물티슈' },
+          { text: '분유', value: '분유' },
+          { text: '생활용품', value: '생활용품' },
+          { text: '세정제', value: '세정제' },
+          { text: '스킨/바디케어', value: '스킨/바디케어' },
+          { text: '임산부', value: '임산부' },
+        ],
+        onFilter: (value, record) => record.category_Name.indexOf(value) === 0,
       },
       {
         title: "소카테고리",
@@ -77,12 +153,14 @@ export default {
         title: "좋은점(글수)",
         dataIndex: 'ProductReview_content',
         key: 'ProductReview_content',
+        scopedSlots: { customRender: 'ProductReview_content' },
         width: '15%',
       },
       {
         title: "안좋은점(글수)",
         dataIndex: 'ProductReview_Content_bad',
         key: 'ProductReview_Content_bad',
+        scopedSlots: { customRender: 'ProductReview_Content_bad' },
         width: '15%',
       },
       {
@@ -91,6 +169,8 @@ export default {
         key: 'rating',
         scopedSlots: { customRender: 'rating' },
         width: '15%',
+        sorter: (a, b) => a.rating - b.rating,
+        sortDirections: ['descend', 'ascend'],
       },
       {
         title: "등록일",
@@ -104,17 +184,29 @@ export default {
         key: 'status',
         scopedSlots: { customRender: 'status' },
       },
-      
+
     ]
     return {
       columns: columns,
-      data: dummys
+      data: dummys,
+      filteredData: dummys,
+      startValue: null,
+      endValue: moment(new Date().toJSON().slice(0, 10).replace(/-/g, '.').substring(0, 10)),
+      endOpen: false,
     }
   }
 
 }
 </script>
 <style>
+.date-picker {
+  margin: 10px 0;
+}
+
+.text-bold {
+  font-weight: 800 !important;
+}
+
 .ant-table-title {
   display: none;
 }
@@ -127,13 +219,16 @@ export default {
   background-color: white;
   text-align: center;
 }
-.ant-table-tbody > tr > td {
+
+.ant-table-tbody>tr>td {
   font-weight: 600 !important;
 }
+
 .ant-rate-star:not(:last-child) {
-    margin-right: 4px;
+  margin-right: 4px;
 }
+
 .v-application ul {
-    padding-left: 0;
+  padding-left: 0;
 }
 </style>
