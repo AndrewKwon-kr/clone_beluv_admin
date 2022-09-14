9a4a9fc00fffe57b93dcc25cda84d9e0491c735e
AndrewKwon <hj.kwon@mayoube.co.kr>
2022-09-14 16:19:34 +0900

    테이블 컴포넌트 분리

--- src/components/ReviewPage/ModalComponent.vue
+++ src/components/ReviewPage/ModalComponent.vue
@@ -3,53 +3,9 @@
     :closable="false" width="80%" height="40%" :ok-button-props="{ style: { display: 'none'} }">
     <div class="modal-container">
       <div class="left-area">
-        <a-table :columns="columns" :data-source="productData" :rowKey="(record, index) => { return index }"
-          :loading="loading">
-          <div slot="url" slot-scope="url">
-            <div v-for="(imageUrl, index) in url" v-bind:key="index">
-              <div v-if="imageUrl">
-                <a-popover trigger="click">
-                  <template slot="content">
-                    <img :src=imageUrl v-bind:style="{width: '600px', height: '400px'}" />
-                  </template>
-                  <img :src=imageUrl v-bind:style="{width: '30px', height: '20px', cursor: 'pointer'}" />
-                </a-popover>
-              </div>
-              <div v-else>
-                <img :src="require('@/assets/ic-beluv.png')" />
-              </div>
-            </div>
-          </div>
-          <div slot="content" slot-scope="content">
-            <a-tooltip placement="bottom">
-              <template slot="title">
-                <span>{{ content }}</span>
-              </template>
-              <span>{{ content.substr(0, 30) }}...</span>
-              <span class="text-bold">({{ content.length }})</span>
-              <v-icon color="#388E3C">mdi-card-text</v-icon>
-            </a-tooltip>
-          </div>
-          <div slot="content_bad" slot-scope="content_bad">
-            <a-tooltip placement="bottom">
-              <template slot="title">
-                <span>{{ content_bad }}</span>
-              </template>
-              <span>{{ content_bad.substr(0, 30) }}...</span>
-              <span class="text-bold" v-if="content_bad">({{ content_bad.length
-              }})</span>
-              <v-icon color="#388E3C">mdi-card-text</v-icon>
-            </a-tooltip>
-          </div>
-          <div slot="rating" slot-scope="rating">
-            <a-rate :value=rating disabled style="fontSize: 10px" />
-          </div>
-          <div slot="gender" slot-scope="gender">
-            <span v-if="gender==='M'">남</span>
-            <span v-else-if="gender==='F'">여</span>
-            <span v-else>모름</span>
-          </div>
-        </a-table>
+        <ProductTableVue 
+          :data="productData"
+        />
       </div>
       <div class="right-area">
         <div class="card-wrapper">
@@ -88,85 +44,19 @@
 </template>
 <script>
 import { headers } from '@/http-api/index'
+import ProductTableVue from './ProductTable.vue';
 
 export default {
   props: ['data', 'visible', 'loading'],
   name: 'modalComponent',
+  components: {
+    ProductTableVue
+  },
   data() {
     const ratingArray = ['5', '4', '3', '2', '1'];
     const colorArray = ['#1976d2', '#303f9f', '#00796b', '#fbc02d', '#e64a19']
-    const columns = [
-      {
-        title: "No",
-        dataIndex: 'no',
-        key: 'no',
-        width: '10%',
-        sorter: (a, b) => a.no - b.no,
-        sortDirections: ['ascend'],
-      },
-      {
-        title: "글쓴이",
-        dataIndex: 'nickName',
-        key: 'nickName',
-        width: '7%',
-      },
-      {
-        title: "이미지",
-        dataIndex: 'url',
-        key: 'url',
-        scopedSlots: { customRender: 'url' },
-        width: '7%',
-      },
-      {
-        title: "좋은점(글수)",
-        dataIndex: 'content',
-        key: 'content',
-        scopedSlots: { customRender: 'content' },
-        width: '15%',
-      },
-      {
-        title: "안좋은점(글수)",
-        dataIndex: 'content_bad',
-        key: 'content_bad',
-        scopedSlots: { customRender: 'content_bad' },
-        width: '15%',
-      },
-      {
-        title: "별점",
-        dataIndex: 'rating',
-        key: 'rating',
-        scopedSlots: { customRender: 'rating' },
-        width: '15%',
-        sorter: (a, b) => a.rating - b.rating,
-        sortDirections: ['descend', 'ascend'],
-      },
-      {
-        title: "아기성별",
-        dataIndex: 'gender',
-        key: 'gender',
-        scopedSlots: { customRender: 'gender' },
-      },
-      {
-        title: "아기생일",
-        dataIndex: 'birth',
-        key: 'birth',
-        scopedSlots: { customRender: 'birth' },
-      },
-      {
-        title: "아기피부",
-        dataIndex: 'skinType',
-        key: 'skinType',
-        scopedSlots: { customRender: 'skinType' },
-      },
-      {
-        title: "등록일",
-        dataIndex: 'createdAt',
-        key: 'createdAt',
-        scopedSlots: { customRender: 'createdAt' },
-      },
-    ]
+
     return {
-      columns: columns,
       productData: [],
       reviewData: {},
       ratingArray: ratingArray,
---
+++ src/components/ReviewPage/ProductTable.vue
@@ -0,0 +1,131 @@
+<template>
+  <a-table :columns="columns" :data-source="data" :rowKey="(record, index) => { return index }"
+    :loading="loading">
+    <div slot="url" slot-scope="url">
+      <div v-for="(imageUrl, index) in url" v-bind:key="index">
+        <div v-if="imageUrl">
+          <a-popover trigger="click">
+            <template slot="content">
+              <img :src=imageUrl v-bind:style="{width: '600px', height: '400px'}" />
+            </template>
+            <img :src=imageUrl v-bind:style="{width: '30px', height: '20px', cursor: 'pointer'}" />
+          </a-popover>
+        </div>
+        <div v-else>
+          <img :src="require('@/assets/ic-beluv.png')" />
+        </div>
+      </div>
+    </div>
+    <div slot="content" slot-scope="content">
+      <a-tooltip placement="bottom">
+        <template slot="title">
+          <span>{{ content }}</span>
+        </template>
+        <span>{{ content.substr(0, 30) }}...</span>
+        <span class="text-bold">({{ content.length }})</span>
+        <v-icon color="#388E3C">mdi-card-text</v-icon>
+      </a-tooltip>
+    </div>
+    <div slot="content_bad" slot-scope="content_bad">
+      <a-tooltip placement="bottom">
+        <template slot="title">
+          <span>{{ content_bad }}</span>
+        </template>
+        <span>{{ content_bad.substr(0, 30) }}...</span>
+        <span class="text-bold" v-if="content_bad">({{ content_bad.length
+        }})</span>
+        <v-icon color="#388E3C">mdi-card-text</v-icon>
+      </a-tooltip>
+    </div>
+    <div slot="rating" slot-scope="rating">
+      <a-rate :value=rating disabled style="fontSize: 10px" />
+    </div>
+    <div slot="gender" slot-scope="gender">
+      <span v-if="gender==='M'">남</span>
+      <span v-else-if="gender==='F'">여</span>
+      <span v-else>모름</span>
+    </div>
+  </a-table>
+</template>
+<script>
+export default {
+  props: ['data'],
+  name: 'productTable',
+  data() {
+    const columns = [
+      {
+        title: "No",
+        dataIndex: 'no',
+        key: 'no',
+        width: '10%',
+        sorter: (a, b) => a.no - b.no,
+        sortDirections: ['ascend'],
+      },
+      {
+        title: "글쓴이",
+        dataIndex: 'nickName',
+        key: 'nickName',
+        width: '7%',
+      },
+      {
+        title: "이미지",
+        dataIndex: 'url',
+        key: 'url',
+        scopedSlots: { customRender: 'url' },
+        width: '7%',
+      },
+      {
+        title: "좋은점(글수)",
+        dataIndex: 'content',
+        key: 'content',
+        scopedSlots: { customRender: 'content' },
+        width: '15%',
+      },
+      {
+        title: "안좋은점(글수)",
+        dataIndex: 'content_bad',
+        key: 'content_bad',
+        scopedSlots: { customRender: 'content_bad' },
+        width: '15%',
+      },
+      {
+        title: "별점",
+        dataIndex: 'rating',
+        key: 'rating',
+        scopedSlots: { customRender: 'rating' },
+        width: '15%',
+        sorter: (a, b) => a.rating - b.rating,
+        sortDirections: ['descend', 'ascend'],
+      },
+      {
+        title: "아기성별",
+        dataIndex: 'gender',
+        key: 'gender',
+        scopedSlots: { customRender: 'gender' },
+      },
+      {
+        title: "아기생일",
+        dataIndex: 'birth',
+        key: 'birth',
+        scopedSlots: { customRender: 'birth' },
+      },
+      {
+        title: "아기피부",
+        dataIndex: 'skinType',
+        key: 'skinType',
+        scopedSlots: { customRender: 'skinType' },
+      },
+      {
+        title: "등록일",
+        dataIndex: 'createdAt',
+        key: 'createdAt',
+        scopedSlots: { customRender: 'createdAt' },
+      },
+    ]
+    return {
+      columns: columns,
+    }
+  },
+}
+</script>
+
