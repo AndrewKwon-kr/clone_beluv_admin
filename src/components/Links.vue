dfa9e031a9bccbddf818c6729e01967e505288d9
AndrewKwon <hj.kwon@mayoube.co.kr>
2022-09-14 17:04:29 +0900

    불필요한 데이터 제거

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -1,24 +1,7 @@
 <template>
   <v-container>
     <v-row align="center">
-      <!-- <v-col
-        v-for="(link, i) in links"
-        :key="i"
-        class="text-center"
-        cols="6"
-        md="auto"
-      >
-        <a
-          :href="link.href"
-          class="text-decoration-none text-uppercase text-caption font-weight-regular"
-          rel="noopener"
-          target="_blank"
-          v-text="link.text"
-        />
-      </v-col> -->
-
       <v-spacer class="hidden-sm-and-down" />
-
       <v-col
         cols="12"
         md="auto"
@@ -37,27 +20,6 @@
 <script>
   export default {
     name: 'Links',
-
-    data: () => ({
-      links: [
-        {
-          href: 'https://vuetifyjs.com/',
-          text: 'Vuetify Docs',
-        },
-        {
-          href: '#',
-          text: 'About Us',
-        },
-        {
-          href: '#',
-          text: 'Blog',
-        },
-        {
-          href: '#',
-          text: 'Licenses',
-        },
-      ],
-    }),
   }
 </script>
 
